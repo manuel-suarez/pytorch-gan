a2f259c13804ea8525c18e2805e9a59f14e56e4c
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 16:31:26 -0600

    Plot results

--- main.py
+++ main.py
@@ -280,4 +280,15 @@ for epoch in range(num_epochs):
 
         iters += 1
 
+plt.figure(figsize=(10,5))
+plt.title("Generator and Discriminator Loss During Training")
+plt.plot(G_losses,label="G")
+plt.plot(D_losses,label="D")
+plt.xlabel("iterations")
+plt.ylabel("Loss")
+plt.legend()
+plt.show()
+plt.savefig("figura2.png")
+plt.close()
+
 print("Done!")
