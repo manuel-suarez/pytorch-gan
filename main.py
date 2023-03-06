33dde89445a18000c71f74c1f41797a07be08363
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 16:38:39 -0600

    Animation

--- main.py
+++ main.py
@@ -291,4 +291,12 @@ plt.show()
 plt.savefig("figura2.png")
 plt.close()
 
+fig = plt.figure(figsize=(8,8))
+plt.axis("off")
+ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
+ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
+ani.save('faces.gif')
+
+
+
 print("Done!")
