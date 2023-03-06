f8b99ef4ac72773d93fb77b8657d1f7c63fedfc0
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 16:30:36 -0600

    Loss functions and optimizers

--- main.py
+++ main.py
@@ -183,4 +183,19 @@ netD.apply(weights_init)
 # Print the model
 print(netD)
 
+# Initialize BCELoss function
+criterion = nn.BCELoss()
+
+# Create batch of latent vectors that we will use to visualize
+#  the progression of the generator
+fixed_noise = torch.randn(64, nz, 1, 1, device=device)
+
+# Establish convention for real and fake labels during training
+real_label = 1.
+fake_label = 0.
+
+# Setup Adam optimizers for both G and D
+optimizerD = optim.Adam(netD.parameters(), lr=lr, betas=(beta1, 0.999))
+optimizerG = optim.Adam(netG.parameters(), lr=lr, betas=(beta1, 0.999))
+
 print("Done!")
