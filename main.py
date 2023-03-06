f3b852050478cfee90413105bab78425b3b896be
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 15:55:24 -0600

    Save figure to file

--- main.py
+++ main.py
@@ -82,5 +82,6 @@ real_batch = next(iter(dataloader))
 plt.figure(figsize=(8,8))
 plt.axis("off")
 plt.title("Training Images")
-plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.savefig('figura1.png', np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+plt.close()
 
