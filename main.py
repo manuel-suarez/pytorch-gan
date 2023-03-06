9a94d9d797877bf2010632264d12f9bf856351f1
Arturo Suárez <manuel.suarez@cimat.mx>
2023-03-06 16:39:06 -0600

    Real vs Fake images

--- main.py
+++ main.py
@@ -297,6 +297,23 @@ ims = [[plt.imshow(np.transpose(i,(1,2,0)), animated=True)] for i in img_list]
 ani = animation.ArtistAnimation(fig, ims, interval=1000, repeat_delay=1000, blit=True)
 ani.save('faces.gif')
 
+# Grab a batch of real images from the dataloader
+real_batch = next(iter(dataloader))
 
+# Plot the real images
+plt.figure(figsize=(15,15))
+plt.subplot(1,2,1)
+plt.axis("off")
+plt.title("Real Images")
+plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=5, normalize=True).cpu(),(1,2,0)))
+
+# Plot the fake images from the last epoch
+plt.subplot(1,2,2)
+plt.axis("off")
+plt.title("Fake Images")
+plt.imshow(np.transpose(img_list[-1],(1,2,0)))
+plt.show()
+plt.savefig('figura3.png')
+plt.close()
 
 print("Done!")
